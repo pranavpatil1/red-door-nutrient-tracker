79d4e08f970dd21f925c71f23880ea63ff62b0fd
Pranav Patil <4pranavpatil@gmail.com>
2023-03-15 18:08:30 -0700

    Added a lot more generating stuff

--- gen_test_data.py
+++ gen_test_data.py
@@ -3,21 +3,6 @@ import csv
 import names
 import random
 
-"""
-LOAD DATA LOCAL INFILE 'user.csv' INTO TABLE user
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE 'item.csv' INTO TABLE item
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS
-(item_name, price_usd);
-
-LOAD DATA LOCAL INFILE 'student.csv' INTO TABLE student
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
-
-LOAD DATA LOCAL INFILE 'community_member.csv' INTO TABLE community_member
-FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
-"""
-
 # # this will create a csv for the file provided
 # parser = argparse.ArgumentParser(
 #                     prog='gen_test_data.py')
@@ -53,14 +38,14 @@ with open ("user.csv", "w") as uf:
     community_wr.writerow(["UID", "Credit Card Num", "Expiration Date", "Verification Code"])
 
     for i in range(2150000, 2150020):
-        writer.writerow([i, get_name(), get_bool(), get_bool(), get_bool(), get_bool(), 10000])
+        writer.writerow([i, get_name(), get_bool(), get_bool(), get_bool(), get_bool()])
         if i < 2150000+2:
-            community_wr.writerow([i, "1111222233334444", "2026-12-00"])
-        elif i< 2150000+5:
-            community_wr.writerow([i, None, None, None])
+            community_wr.writerow([i, "1111222233334444", "2026-12-01", "123"])
         else:
             student_wr.writerow([i, 1000])
 
+student_f.close()
+community_mb_f.close()
 
 menu_items = [
     ["Avocado Toast", 6.25],
@@ -80,4 +65,47 @@ with open ("item.csv", "w") as uf:
     writer.writerow(["Item Name", "Price (USD)"])
 
     for item in menu_items:
+        writer.writerow(item)
+
+
+orders = [
+    [i+1, 2150000+i, f"2023-03-15 12:{i+10}:01"] for i in range(30)
+]
+
+with open ("orders.csv", "w", newline='') as uf:
+    # create users
+    writer = csv.writer(uf)
+    """
+    order_id        SERIAL,
+    uid             INT REFERENCES user(uid),
+    order_time      TIMESTAMP NOT NULL, 
+    """
+    writer.writerow(["Order ID", "UID", "Order Time"])
+
+    for item in orders:
+        writer.writerow(item)
+
+orders_items = []
+
+for i in range(1, 11):
+    orders_items.append([i, ((i-1)%4)+1])
+
+for i in range(11, 31):
+    if i % 2 == 0:
+        orders_items.append([i, 1])
+        orders_items.append([i, 3])
+    else:
+        orders_items.append([i, 2])
+        orders_items.append([i, 4])
+
+
+with open ("orders_items.csv", "w") as f:
+    writer = csv.writer(f)
+    """
+    order_id        BIGINT UNSIGNED REFERENCES orders(order_id),
+    item_id         BIGINT UNSIGNED REFERENCES item(item_id),
+    """
+    writer.writerow(["Order ID", "Item ID"])
+
+    for item in orders_items:
         writer.writerow(item)
---
+++ load-data.sql
@@ -0,0 +1,25 @@
+LOAD DATA LOCAL INFILE 'user.csv' INTO TABLE user
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'item.csv' INTO TABLE item
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS
+(item_name, price_usd);
+
+LOAD DATA LOCAL INFILE 'student.csv' INTO TABLE student
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'community_member.csv' INTO TABLE community_member
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS
+(@id, @cnum, @edate, @vcode)
+SET 
+    uid = @id,
+    credit_card_num = NULLIF(@cnum,''),
+    expiration_date = NULLIF(@edate,''),
+    verification_code = NULLIF(@vcode,'');
+
+
+LOAD DATA LOCAL INFILE 'orders.csv' INTO TABLE orders
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'orders_items.csv' INTO TABLE orders_items
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\r\n' IGNORE 1 ROWS;
