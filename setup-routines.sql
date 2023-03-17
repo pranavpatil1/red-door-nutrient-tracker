fcbe1d55bfee002da5c4e2aaed33407825d54ac6
Pranav Patil <4pranavpatil@gmail.com>
2023-03-17 00:30:06 -0700

    added routine line

--- setup-routines.sql
+++ setup-routines.sql
@@ -3,7 +3,9 @@
 
 -- Procedures
 DELIMITER !
-CREATE PROCEDURE add_to_order(username VARCHAR(20), password VARCHAR(20))
+-- this will create a completely new order
+-- item_ids is a string of a list of items like '1,2,5,7'   
+CREATE PROCEDURE create_order(username VARCHAR(20), order_id BIGINT UNSIGNED, item_ids TEXT)
 BEGIN
   DECLARE salt CHAR (8);
 
