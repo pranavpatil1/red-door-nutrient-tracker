63ab1d3557891bab7569ac0795648bef11640d3c
nisha2003 <56770365+nisha2003@users.noreply.github.com>
2023-03-17 05:31:48 -0700

    Update setup-routines.sql

--- setup-routines.sql
+++ setup-routines.sql
@@ -1,5 +1,69 @@
 -- UDFs
 
+DELIMITER !
+CREATE FUNCTION protein_in_item (
+    given_tem_id SERIAL
+) RETURNS NUMERIC(6, 2) DETERMINISTIC
+BEGIN
+    DECLARE amount NUMERIC (6, 2);
+
+    SELECT SUM(protein) INTO amount
+    FROM (  SELECT protein
+            FROM ingr_details NATURAL JOIN recipe
+            WHERE item_id = given_tem_id ) AS temp;
+
+    RETURN amount;
+END !
+DELIMITER ;
+
+DELIMITER !
+CREATE FUNCTION carbs_in_item (
+    given_tem_id SERIAL
+) RETURNS NUMERIC(6, 2) DETERMINISTIC
+BEGIN
+    DECLARE amount NUMERIC (6, 2);
+
+    SELECT SUM(carbs) INTO amount
+    FROM (  SELECT carbs
+            FROM ingr_details NATURAL JOIN recipe
+            WHERE item_id = given_tem_id) AS temp;
+
+    RETURN amount;
+END !
+DELIMITER ;
+
+DELIMITER !
+CREATE FUNCTION fats_in_item (
+    given_tem_id SERIAL
+) RETURNS NUMERIC(6, 2) DETERMINISTIC
+BEGIN
+    DECLARE amount NUMERIC (6, 2);
+
+    SELECT SUM(fats) INTO amount
+    FROM (  SELECT fats
+            FROM ingr_details NATURAL JOIN recipe
+            WHERE item_id = given_tem_id) AS temp;
+
+    RETURN amount;
+END !
+DELIMITER ;
+
+DELIMITER !
+CREATE FUNCTION sugars_in_item (
+    given_tem_id SERIAL
+) RETURNS NUMERIC(6, 2) DETERMINISTIC
+BEGIN
+    DECLARE amount NUMERIC (6, 2);
+
+    SELECT SUM(sugars) INTO amount
+    FROM (  SELECT sugars
+            FROM ingr_details NATURAL JOIN recipe
+            WHERE item_id = given_tem_id) AS temp;
+
+    RETURN amount;
+END !
+DELIMITER ;
+
 
 -- Procedures
 DELIMITER !
