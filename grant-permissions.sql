8c9fd9e6af67944a3f6a4131ecd6100b91be32ee
Pranav Patil <4pranavpatil@gmail.com>
2023-03-16 19:13:00 -0700

    Set up basic CLI options and set up login from CLI

--- grant-permissions.sql
+++ grant-permissions.sql
@@ -1,5 +1,6 @@
-CREATE USER 'appadmin'@'localhost' IDENTIFIED BY 'adminpw';
-CREATE USER 'appclient'@'localhost' IDENTIFIED BY 'clientpw';
+-- issue with mysql on windows
+CREATE USER 'appadmin'@'localhost' IDENTIFIED WITH mysql_native_password BY 'adminpw';
+CREATE USER 'appclient'@'localhost' IDENTIFIED WITH mysql_native_password BY 'clientpw';
 
 -- Can add more users or refine permissions
 GRANT ALL PRIVILEGES ON final.* TO 'appadmin'@'localhost';
