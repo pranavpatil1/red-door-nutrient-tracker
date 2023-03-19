9aed6d576e5e90f39786a7e88f8826269e167307
nisha2003 <56770365+nisha2003@users.noreply.github.com>
2023-03-18 19:45:36 -0700

    Create load-data-mac.sql

---
+++ load-data-mac.sql
@@ -0,0 +1,36 @@
+CALL sp_add_user('hovik', 'ilovesql');
+CALL sp_add_user('pranav', 'ilovefood');
+CALL sp_add_user('nisha', 'ilovesoup');
+CALL sp_add_user('jose', 'ilovereddoor');
+
+LOAD DATA LOCAL INFILE 'user.csv' INTO TABLE user
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'item.csv' INTO TABLE item
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS
+(item_name, price_usd);
+
+LOAD DATA LOCAL INFILE 'student.csv' INTO TABLE student
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'community_member.csv' INTO TABLE community_member
+FIELDS TERMINATED BY ',' LINES TERMINATED BY '\n' IGNORE 1 ROWS
+(@id, @cnum, @edate, @vcode)
+SET 
+    uid = @id,
+    credit_card_num = NULLIF(@cnum,''),
+    expiration_date = NULLIF(@edate,''),
+    verification_code = NULLIF(@vcode,'');
+
+
+LOAD DATA LOCAL INFILE 'orders.csv' INTO TABLE orders
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'orders_items.csv' INTO TABLE orders_items
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'ingr_details.csv' INTO TABLE ingr_details
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+
+LOAD DATA LOCAL INFILE 'recipe.csv' INTO TABLE recipe
+FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
